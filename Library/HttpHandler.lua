86117cb90dfad0fcbe228b482b452d3c51384df9
ThunderGemios10 <thundergemios10@gmail.com>
2020-09-24 11:41:32 +0800

    Update HttpHandler.lua

--- Library/HttpHandler.lua
+++ Library/HttpHandler.lua
@@ -1,9 +1,3 @@
---[[
-	HttpsHandler.lua
-	--------------------
-	Author(s): AdministratorGnar
---]]
-
 -- FLUX
 local require = shared.import("require")
 
@@ -11,41 +5,53 @@ local require = shared.import("require")
 local Players = game:GetService("Players")
 local HttpService = game:GetService("HttpService")
 
--- VARIABLES
-local messagePrefix = "[HttpHandler] "
-
 -- SCRIPT
-local HttpHandler = {} 
-	
-	-- LOCAL
-	
-	-- FUNCTIONS
-	function HttpHandler:GetAsyncWithPcall(url, nocache, headers)
-		local success, message = pcall(function()
-			return HttpService:GetAsync(url or "", nocache or false, headers or nil)
-		end)
-		return success, message
-	end
-	
-	function HttpHandler:PostAsyncWithPcall(url, data, content_type, compress, headers)
-		local success, message = pcall(function()
-			return HttpService:PostAsync(url or "", data or "", content_type or Enum.HttpContentType.ApplicationJson, compress or false, headers or nil)
-		end)
-		
-		if not success then
-			warn(messagePrefix.."ERROR HANDLED: "..message)
-		end
-		
-		return success, message
-	end
-	
-	function HttpHandler:JSONEncode(input)
-		return HttpService:JSONEncode(input)
-	end
+local HttpHandler = {}
+HttpHandler.__index = HttpHandler
 
-	function HttpHandler:JSONDecode(input)
-		return HttpService:JSONDecode(input)
+-- LOCAL
+
+-- FUNCTIONS
+function HttpHandler.new()
+	local self = setmetatable({
+		--@@ PUBLIC
+		
+		--@@ PRIVATE
+		_messagePrefix = "[HttpHandler] ",
+	}, HttpHandler)
+end
+
+function HttpHandler:GetAsyncWithPcall(url, nocache, headers)
+	local success, message = pcall(function()
+		return HttpService:GetAsync(url or "", nocache or false, headers or nil)
+	end)
+	
+	if not success then
+		warn(self._messagePrefix.."ERROR HANDLED: "..message)
 	end
+	
+	return success, message
+end
+
+function HttpHandler:PostAsyncWithPcall(url, data, content_type, compress, headers)
+	local success, message = pcall(function()
+		return HttpService:PostAsync(url or "", data or "", content_type or Enum.HttpContentType.ApplicationJson, compress or false, headers or nil)
+	end)
+	
+	if not success then
+		warn(self._messagePrefix.."ERROR HANDLED: "..message)
+	end
+	
+	return success, message
+end
+
+function HttpHandler:JSONEncode(input)
+	return HttpService:JSONEncode(input)
+end
+
+function HttpHandler:JSONDecode(input)
+	return HttpService:JSONDecode(input)
+end
 
 -- RETURN
-return HttpHandler
+return HttpHandler.new()
